2697e9022b3c351530028104e9c2c52fdf961526
Mohammad <zia.ro@live.com>
2022-10-02 13:08:27 -0400

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
+// ./src/index.js
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
 import "bootstrap/dist/css/bootstrap.min.css";
+import  thunkMiddleware  from "redux-thunk";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import App from "./App";
+import catsReducer from "./features/cats/catsSlice.js";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+
+const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware))
+const store = createStore( catsReducer, composedEnhancer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
